a861b9450d72af7115d563682cefde1dc7351984
nuttakit <petch.zzk@hotmail.com>
2024-02-11 19:33:03 +0700

    add indicator manager

---
+++ Project-J/Assets/Script/DistanceCalcualtor/TestDistanceCalulator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDistanceCalulator : MonoBehaviour
+{
+    [SerializeField] GameObject selfGameObject;
+    [SerializeField] GameObject targerGameObject;
+    [SerializeField] float distanceValue = 10;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Vector3 selfPos = selfGameObject.transform.position;
+            Vector3 targetPos = targerGameObject.transform.position;
+            bool x = DistanceCalculator.CheckDistance(selfPos, targetPos, distanceValue);
+            Debug.Log(x);
+            Debug.Log(DistanceCalculator.GetDistance(selfPos, targetPos));
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            IndicatorManager.instance.showRangeIndicator(selfGameObject, 10);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            IndicatorManager.instance.showRangeIndicator(selfGameObject, 5);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            IndicatorManager.instance.hideRangeIndicator();
+        }
+    }
+}
